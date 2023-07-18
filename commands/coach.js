e29def1d4fb4c82fb234400d32a6a32ada98da57
Andre Correia <91163660+andremcorreia@users.noreply.github.com>
2023-07-18 10:38:06 +0100

    Update coach.js

--- commands/coach.js
+++ commands/coach.js
@@ -9,16 +9,6 @@ module.exports = {
 
         console.log( new Discord.Presence(ownerID).status)
 
-        //const Availability = message.guild.members.fetch(ownerID);
-        //const fetchUser = async id => client.users.fetch(id);
-        //console.log(fetchUser);
- 
-
-        //})//message.guild.ownerID.//.status//.presence//.status
-        //console.log(Availability);
-        //console.log(message.member);
-
         message.channel.send("unavailable check roblox or fortnite?");
-        
     }
 }
